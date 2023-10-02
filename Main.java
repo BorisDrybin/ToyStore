0e464703c9372313efe6bb7594adc3f63a1a2353
Boris Drybin <makoh93@gmail.com>
2023-10-02 20:16:06 +0300

    code added

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Shop shop = new Shop();
+            int id = 0;
+            shop.addNewToy(id++, "Кубик Рубика", 45);
+            shop.addNewToy(id++, "Монополия", 50);
+            shop.addNewToy(id++, "Конструктор", 50);
+            shop.addNewToy(id++, "Кукла Барби", 55);
+            shop.addNewToy(id++, "Елочка", 55);
+
+
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Разыграна игрушка: " + shop.GetToy().toString());
+            }
+
+            String pathFile = "toys.txt";
+            FileWriter fw = new FileWriter(pathFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PriorityQueue<Toy> queueToys = shop.getQueueToys();
+            while(queueToys.size() > 0){
+                Toy t = queueToys.element();
+                bw.write(t.toString() + System.lineSeparator());
+                queueToys.remove();
+            }
+            bw.close();
+            fw.close();
+        }
+        catch(IOException e){
+            System.out.println(e.toString());
+        }
+        catch (RuntimeException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
